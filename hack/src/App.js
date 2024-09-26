11b64f6fc9009fe62df9abc28fb01db6f146cb05
Harshitr03 <rawat.harshit03@gmail.com>
2024-09-26 11:49:00 +0530

    login

--- hack/src/App.js
+++ hack/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import './App.css';
 import Main from './pages/main'
 import Home from './pages/home';
 import Login from './pages/login';
+
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/main" element={<Main/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
